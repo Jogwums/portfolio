d08c89b034d137bf8064723471520bef8e22d527
Jonathan Ogwumike <jonathanogwumike@gmail.com>
2023-04-25 11:37:39 +0100

    new covid 19 dash added

--- src/pages/Da.js
+++ src/pages/Da.js
@@ -10,6 +10,7 @@ import image2 from '../assets/img/pbiprojects/pbi2.JPG'
 import image3 from '../assets/img/pbiprojects/pbi3.JPG'
 import image4 from '../assets/img/pbiprojects/pbi4.JPG'
 import image6 from '../assets/img/pbiprojects/pbi6.JPG'
+import image7 from '../assets/img/pbiprojects/pbi7.JPG'
 
 const Da = ({type, text}) => {
 
@@ -42,12 +43,18 @@ const Da = ({type, text}) => {
          link={"https://app.powerbi.com/view?r=eyJrIjoiNzBjYTVlYzEtZDExMS00ZmRiLTg2MTQtMjY1ZGU1YWY4YTg5IiwidCI6ImUyOGUyY2RlLWYzN2YtNDVkYi1iYWNlLWE0YjFkZDI3NzYyMyJ9"}
          />
          </div>
-         <div className="row card-deck">
+         <div className="card-deck">
          <Card
         image={image6}
         text={"Nigerian Presidential Elections 2023 Analysis"}
         text2={"Dashboard shows Election results and Regional Analysis"}
          link={"https://app.powerbi.com/view?r=eyJrIjoiZDNjM2UxNTktMTdiYy00OTU4LTkyYjQtMWI4NGVjNzBjMDNjIiwidCI6ImUyOGUyY2RlLWYzN2YtNDVkYi1iYWNlLWE0YjFkZDI3NzYyMyJ9"}
+         />
+         <Card
+        image={image7}
+        text={"Covid 19 Dashboard 2023"}
+        text2={"Dashboard shows Global covid Analysis"}
+         link={"https://app.powerbi.com/view?r=eyJrIjoiZDE0YzU5NmMtNDdlZC00NmYyLThkOWEtNTE1NGYyMDhmNDZjIiwidCI6ImUyOGUyY2RlLWYzN2YtNDVkYi1iYWNlLWE0YjFkZDI3NzYyMyJ9"}
          />
         </div>
         </>
