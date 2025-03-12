4905c7867a95cfcf264f14abf2363e4418aebe96
Jonathan Ogwumike <49166200+Jogwums@users.noreply.github.com>
2025-03-12 00:22:28 +0000

    updates to css on portfolio page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,33 +1,26 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
-
-// component
-// import Example from '../ButtonCollapse'
-
-import {FaLinkedin, FaGithub, FaKaggle } from 'react-icons/fa'
+import { FaLinkedin, FaGithub, FaKaggle } from 'react-icons/fa';
 
 const FooterDetails = () => {
     return (
-    <>
-      <Navbar className="footer py-1 bg-black text-white flex">
-              <div className='container'>
-                {/* <!-- social icon --> */}
-              <div className="socials float-left">
-                <ul className="list-unstyled mt-3 position-relative zindex-1">
-                  <li className='mb-3'><a className='text-white' href="https://github.com/Jogwums"><FaGithub size={`2em`} /></a></li>
-                  <li className='mb-3'><a className='text-white' href="https://www.kaggle.com/jogwums"><FaKaggle size={`2em`} /></a></li>
-                  <li className='mb-3'><a className='text-white' href="https://www.linkedin.com/in/jonathan-ogwumike012016/">
-                  <FaLinkedin size={`2em`} /></a></li>
-              </ul>
-            </div>
-            {/* <!-- /social icon --> */}
-              </div>
-            <div className="footer-container px-5 bg-black">
-              <p className="footer-text m-0 small">&copy; Jonathan 2021</p>
+        <Navbar className="footer py-1 bg-black text-white">
+            <div className='container'>
+                {/* Social icons */}
+                <div className="socials float-left">
+                    <ul className="list-unstyled mt-3 position-relative zindex-1">
+                        <li className='mb-3'><a className='text-white' href="https://github.com/Jogwums"><FaGithub size={`2em`} /></a></li>
+                        <li className='mb-3'><a className='text-white' href="https://www.kaggle.com/jogwums"><FaKaggle size={`2em`} /></a></li>
+                        <li className='mb-3'><a className='text-white' href="https://www.linkedin.com/in/jonathan-ogwumike012016/"><FaLinkedin size={`2em`} /></a></li>
+                    </ul>
+                </div>
+                {/* Footer text */}
+                <div className="footer-container px-5 bg-black">
+                    <p className="footer-text m-0 small">&copy; Jonathan 2025</p>
+                </div>
             </div>
         </Navbar>
-    </>
-  )
-}
+    );
+};
 
 export default FooterDetails;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,57 +1,43 @@
-import React from 'react'
-
-import Navigation from '../components/navbar'
-import Footer from '../components/Footer'
-
-// styling
-// import Card from '../components/CardNoImage'
-import ImageCard from '../components/Card'
-
-// components
-import { Button, Container, Row, Col } from 'react-bootstrap'
+import React from 'react';
+import Navigation from '../components/navbar';
+import Footer from '../components/Footer';
+import ImageCard from '../components/Card';
+import { Button, Container, Row, Col } from 'react-bootstrap';
 
 const Portfolio = () => {
-  return (
-    <>
-        <Navigation />
-            <Container fluid className='m-0 p-0'>
-            <Row className='p-0 mb-5'>
-              <Col sm={12} md={6} className='m-0 p-0'>
-                <div className="">
-                      <div className='portfolio-card-text-da'>
-                        {/* <h2>Data Portfolio</h2> */}
-                        <ImageCard 
-                        style={{width:"50%"}}
-                        image={"https://picsum.photos/200.webp?grayscale?random=2"} text={"Data Portfolio"} 
-                        text2={"View projects created using tools like power BI, python and Tableau"}
-                        link={"/data"}
-                         />
-                        <Button href='/data'>View</Button>
-                   </div>
-                </div>
-              </Col>
-                {/* design section */}
-              <Col sm={12} md={6} className='m-0 p-0'>
-                <div className="">
-                      <div className='portfolio-card-text-ds '>
-                        {/* <h2>Design Portfolio</h2> */}
-                        <div className=''>
-                          <ImageCard 
-                          image={"https://picsum.photos/200.webp?grayscale?random=3"} text={"Design Portfolio"} 
-                          text2={"Reactjs projects using libraries and APIs"}
-                          link={"/design"}
-                           />
-                      </div>
-                          <Button href='/design'>View</Button>
-                    </div>
-                </div>
-              </Col>
-              </Row>
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+            <Navigation />
+            <Container fluid className='m-0 p-0 flex-grow-1'>
+                <Row className='p-0 mb-5'>
+                    <Col sm={12} md={6} className='m-0 p-0'>
+                        <div className="portfolio-card-text-da">
+                            <ImageCard
+                                style={{ width: "50%" }}
+                                image={"https://picsum.photos/200.webp?grayscale?random=2"}
+                                text={"Data Portfolio"}
+                                text2={"View projects created using tools like power BI, python and Tableau"}
+                                link={"/data"}
+                            />
+                            <Button href='/data'>View</Button>
+                        </div>
+                    </Col>
+                    <Col sm={12} md={6} className='m-0 p-0'>
+                        <div className="portfolio-card-text-ds">
+                            <ImageCard
+                                image={"https://picsum.photos/200.webp?grayscale?random=3"}
+                                text={"Design Portfolio"}
+                                text2={"Reactjs projects using libraries and APIs"}
+                                link={"/design"}
+                            />
+                            <Button href='/design'>View</Button>
+                        </div>
+                    </Col>
+                </Row>
             </Container>
-        <Footer/>
-    </>
+            <Footer />
+        </div>
+    );
+};
 
-  )
-}
-
-export default Portfolio
+export default Portfolio;
